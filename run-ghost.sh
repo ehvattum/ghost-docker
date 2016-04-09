783e32daf02bf4e172e118d9b09dc8d938ad09b5
Erik Hvattum <erik.hvattum@gmail.com>
2016-04-09 16:34:19 +0200

    adding stuff

--- run-ghost.sh
+++ run-ghost.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+
 _theme_source_destination="${HOME}/content/themes/default"
 
-if [ -d ${_theme_source_destination} ]; then
-    cd ${_theme_source_destination} && git pull origin master
-    cd $HOME
-else
-    git clone ${THEME_SOURCE} $HOME/content/themes/default
-fi
+#if [ -d ${_theme_source_destination} ]; then
+#    cd ${_theme_source_destination} && git pull origin master
+#    cd $HOME
+#else
+#    git clone ${THEME_SOURCE} $HOME/content/themes/default
+#fi
 
 chown -R ghost /var/www/ghost
 su ghost -c "npm start"
